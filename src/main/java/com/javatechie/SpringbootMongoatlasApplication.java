79fb713d691336b2b1e6f38f8ad9070e50a77a64
lucasdipaula <lucas.dipaula9969@gmail.com>
2023-05-30 16:50:41 -0300

    feat: Create Task CRUD

---
+++ src/main/java/com/javatechie/SpringbootMongoatlasApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoatlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoatlasApplication.class, args);
+	}
+
+}
